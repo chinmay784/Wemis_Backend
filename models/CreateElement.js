efcbebc8969b2ee3b851e40c4f97c588eac196fe
chinmaypuhan <chinmaypuhan420@gmail.com>
2025-08-25 10:39:27 +0530

    edit

--- models/CreateElement.js
+++ models/CreateElement.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const CreateElement = new mongoose.Schema({
     elementCategoryModelId: {
         type: mongoose.Types.ObjectId,
+        ref:"ElementCategory"
     },
     elementName: {
         type: String,
--- models/CreateElementCategory.js
+++ models/CreateElementCategory.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 const ElementCategory = new mongoose.Schema({
     elementBrandModelId:{
         type:mongoose.Types.ObjectId,
+        ref:"Brandmodel"
     },
     elementCategoryName:{
         type:String,
