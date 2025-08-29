fdc8e20c733751abbc47e1587cfaff85eb7ef0f4
chinmaypuhan <chinmaypuhan420@gmail.com>
2025-08-29 17:39:38 +0530

    edit

--- models/CreateElement.js
+++ models/CreateElement.js
@@ -4,17 +4,21 @@ const mongoose = require("mongoose");
 const CreateElement = new mongoose.Schema({
     elementCategoryModelId: {
         type: mongoose.Types.ObjectId,
-        ref:"ElementCategory"
+        ref: "ElementCategory"
     },
     elementName: {
         type: String,
-        trim:true,
+        trim: true,
     },
     is_Vltd: {
         type: Boolean,
         default: false,
     },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
 });
 
 
-module.exports = mongoose.model("CreateElement",CreateElement);
+module.exports = mongoose.model("CreateElement", CreateElement);
