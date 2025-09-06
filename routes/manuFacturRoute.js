13d5cac73760a40bd51be1bde9fc137ee830154c
chinmaypuhan <chinmaypuhan420@gmail.com>
2025-09-06 12:49:03 +0530

    edit

--- routes/manuFacturRoute.js
+++ routes/manuFacturRoute.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const { authMiddelWere } = require('../middelwere/authMiddelWere');
-const { createDistributor ,fetchDistributor ,deletedistributor} = require('../controllers/manuFactrerController');
+const { createDistributor ,fetchDistributor ,deleteDistributor} = require('../controllers/manuFactrerController');
 const { upload } = require('../config/cloudinary');
 const router = express.Router();
 
 router.post("/createDistributor",upload.none(),authMiddelWere,createDistributor);
 router.post("/fetchDistributor",authMiddelWere,fetchDistributor);
-router.post("/deleteDistributor",authMiddelWere,deletedistributor)
+router.post("/deleteDistributor",authMiddelWere,deleteDistributor)
 
 
 module.exports = router;
