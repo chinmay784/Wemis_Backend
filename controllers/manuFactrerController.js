3239ec71fec83cf7ae7b48ba79cb746c0d92b0e9
chinmaypuhan <chinmaypuhan420@gmail.com>
2025-09-06 12:47:46 +0530

    edit

--- controllers/manuFactrerController.js
+++ controllers/manuFactrerController.js
@@ -2,41 +2,41 @@ const Distributor = require("../models/CreateDistributor")
 const User = require("../models/UserModel")
 
 
-exports.createDistributor = async (req, res) =>{
+exports.createDistributor = async (req, res) => {
     try {
         const userId = req.user.userId
 
-        if(!userId){
+        if (!userId) {
             return res.status(200).json({
-                sucess:false,
-                message:"Please Provide userId"
+                sucess: false,
+                message: "Please Provide userId"
             })
         }
 
-        const {business_Name , contact_Person_Name ,email , gender , mobile , date_of_Birth , age , Map_Device_Edit , pAN_Number , occupation , advance_Payment , languages_Known , country , state , district , address } = req.body;
+        const { business_Name, contact_Person_Name, email, gender, mobile, date_of_Birth, age, Map_Device_Edit, pAN_Number, occupation, advance_Payment, languages_Known, country, state, district, address } = req.body;
 
 
-        if(!business_Name || !contact_Person_Name || !email || !gender || !mobile || !date_of_Birth || !age || !Map_Device_Edit || !pAN_Number || !occupation || !advance_Payment || !languages_Known || !country || !state || !district || !address){
+        if (!business_Name || !contact_Person_Name || !email || !gender || !mobile || !date_of_Birth || !age || !Map_Device_Edit || !pAN_Number || !occupation || !advance_Payment || !languages_Known || !country || !state || !district || !address) {
             return res.status(200).json({
-                sucess:false,
-                message:"please provide all fields"
+                sucess: false,
+                message: "please provide all fields"
             })
         };
 
         // find allready present
 
-        const distributor = await Distributor.findOne({email:email});
+        const distributor = await Distributor.findOne({ email: email });
 
-        if(distributor){
+        if (distributor) {
             return res.status(200).json({
-                sucess:false,
-                message:"Distributor Already Exist"
+                sucess: false,
+                message: "Distributor Already Exist"
             })
         }
 
 
         const dist = new Distributor({
-            manufacturId:userId,
+            manufacturId: userId,
             business_Name,
             contact_Person_Name,
             email,
@@ -51,7 +51,7 @@ exports.createDistributor = async (req, res) =>{
             languages_Known,
             country,
             state,
-            district,address
+            district, address
         });
 
 
@@ -59,61 +59,99 @@ exports.createDistributor = async (req, res) =>{
 
         // create user in USER Collection
         const distSaveInUser = new User({
-            distributorId:dist._id,
-            email:email,
-            password:mobile,
-            role:"distibutor",
+            distributorId: dist._id,
+            email: email,
+            password: mobile,
+            role: "distibutor",
         })
 
         await distSaveInUser.save();
 
         return res.status(200).json({
-            sucess:true,
-            message:"Distributor Created SucessFully"
+            sucess: true,
+            message: "Distributor Created SucessFully"
         })
 
 
     } catch (error) {
-        console.log(error,error.message);
+        console.log(error, error.message);
         return res.status(500).json({
-            sucess:false,
-            message:"Server Error in createDistributor"
+            sucess: false,
+            message: "Server Error in createDistributor"
         })
     }
 }
 
 
-exports.fetchDistributor = async (req, res) =>{
+exports.fetchDistributor = async (req, res) => {
     try {
         const userId = req.user.userId;
 
-        if(!userId){
+        if (!userId) {
             return res.status(200).json({
-                sucess:false,
-                message:"Please Provide userId"
+                sucess: false,
+                message: "Please Provide userId"
             })
         }
 
         // find in distributor 
-        const distributor = await Distributor.find({manufacturId:userId});
+        const distributor = await Distributor.find({ manufacturId: userId });
 
-        if(!distributor){
+        if (!distributor) {
             return res.status(200).json({
-                sucess:false,
-                message:"No Distributor Found"
+                sucess: false,
+                message: "No Distributor Found"
             })
         }
 
         return res.status(200).json({
-            sucess:true,
-            message:'Distributor fetched Sucessfully',
-            distributor, 
+            sucess: true,
+            message: 'Distributor fetched Sucessfully',
+            distributor,
         })
     } catch (error) {
-        console.log(error,error.message);
+        console.log(error, error.message);
         return res.status(500).json({
-            sucess:false,
-            message:"server error in Fetch Distributor "
+            sucess: false,
+            message: "server error in Fetch Distributor "
+        })
+    }
+}
+
+
+exports.deleteDistributor = async (req, res) => {
+    try {
+        const userId = req.user.userId;
+
+        if (!userId) {
+            return res.status(200).json({
+                sucess: false,
+                message: "Please Provide userId"
+            })
+        }
+
+        const { id } = req.body;
+
+        if (!id) {
+            return res.status(200).json({
+                sucess: false,
+                message: "Please Provide id"
+            })
+        };
+
+
+        const deletedistributor = await Distributor.findByIdAndDelete({ _id: id });
+
+        return res.status(200).json({
+            sucess: true,
+            message: "Distributor Deleted SucessFully"
+        })
+
+    } catch (error) {
+        console.log(error, error.message);
+        return res.status(500).json({
+            sucess: false,
+            message: "server error in deleteDistributor "
         })
     }
 }
--- routes/manuFacturRoute.js
+++ routes/manuFacturRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { authMiddelWere } = require('../middelwere/authMiddelWere');
-const { createDistributor ,fetchDistributor } = require('../controllers/manuFactrerController');
+const { createDistributor ,fetchDistributor ,deletedistributor} = require('../controllers/manuFactrerController');
 const { upload } = require('../config/cloudinary');
 const router = express.Router();
 
 router.post("/createDistributor",upload.none(),authMiddelWere,createDistributor);
-router.post("/fetchDistributor",authMiddelWere,fetchDistributor)
+router.post("/fetchDistributor",authMiddelWere,fetchDistributor);
+router.post("/deleteDistributor",authMiddelWere,deletedistributor)
 
 
 module.exports = router;
